6681d9ca7e6277acdfac9eea050ec2b80c2101bd
LIU Zhao <liuzhaomax@163.com>
2022-09-29 12:46:45 +1000

    jwt + itcpt

--- internal/core/cryptox.go
+++ internal/core/cryptox.go
@@ -10,10 +10,8 @@ import (
 	"encoding/base64"
 	"encoding/pem"
 	"fmt"
-	"github.com/dgrijalva/jwt-go"
 	"io"
 	"os"
-	"time"
 )
 
 func MD5(byt []byte) string {
@@ -138,26 +136,4 @@ func RSAEncrypt(publicKey *rsa.PublicKey, str string) (string, error) {
 	}
 	encryptedStr := base64.StdEncoding.EncodeToString(encryptedBytes)
 	return encryptedStr, nil
-}
-
-func GenerateToken(text string, duration time.Duration) (string, error) {
-	at := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"uid": text,
-		"exp": time.Now().Add(duration).Unix(),
-	})
-	token, err := at.SignedString([]byte(ctx.JWTSecret))
-	if err != nil {
-		return "", err
-	}
-	return token, nil
-}
-
-func ParseToken(token string) (string, error) {
-	claim, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
-		return []byte(ctx.JWTSecret), nil
-	})
-	if err != nil {
-		return "", err
-	}
-	return claim.Claims.(jwt.MapClaims)["uid"].(string), nil
 }
