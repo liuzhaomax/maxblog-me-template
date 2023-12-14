f313e35a7e3fc98a01d24655f7fa74623faa14f9
LIUZHAO <liuzhaomax@163.com>
2023-12-14 11:36:25 +0800

    1.21

--- internal/cmd/env/dev.go
+++ internal/cmd/env/dev.go
@@ -1,11 +1,1 @@
-//go:build !prod
-
-package env
-
-import "flag"
-
-func LoadEnv() *string {
-	config := flag.String("c", "env/dev.yaml", "配置文件")
-	flag.Parse()
-	return config
-}
+//go:build !prodpackage envimport "flag"func LoadEnv() *string {    config := flag.String("c", "env/dev.yaml", "配置文件")    flag.Parse()    return config}
